1c4d6926f237065c0fa9585da45ae4f104b170a7
FelipeCruz <dacruzfe@bol.com.br>
2024-03-26 14:38:39 -0300

    atualização de arquivos e inclusão do nuget

--- EXEMPLOFUNDAMENTOS/ExemploFundamentos/obj/Debug/net6.0/EXEMPLOFUNDAMENTOS.AssemblyInfo.cs
+++ EXEMPLOFUNDAMENTOS/ExemploFundamentos/obj/Debug/net6.0/EXEMPLOFUNDAMENTOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EXEMPLOFUNDAMENTOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2265ef722ee8cd6991b782b123ad22642bc15011")]
 [assembly: System.Reflection.AssemblyProductAttribute("EXEMPLOFUNDAMENTOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EXEMPLOFUNDAMENTOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
